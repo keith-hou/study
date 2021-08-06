a34e00a4e9d1138613b2360636224bb8b1076ef4
houyingjie <houyingjie@tal.com>
2021-08-06 16:56:02 +0800

    提交代码

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"study/cmd"
+)
+
+func main1() {
+	var name string
+	flag.Parse()
+	args := flag.Args()
+	if len(args) <= 0 {
+		return
+	}
+
+	switch args[0] {
+	case "go":
+		goCmd := flag.NewFlagSet("go", flag.ExitOnError)
+		goCmd.StringVar(&name, "name", "go语言", "帮助信息")
+		_ = goCmd.Parse(args[1:])
+	case "php":
+		phpCmd := flag.NewFlagSet("php", flag.ExitOnError)
+		phpCmd.StringVar(&name, "name", "php语言", "帮助信息")
+		_ = phpCmd.Parse(args[1:])
+	}
+	log.Printf("name:%s", name)
+}
+
+func main() {
+	err := cmd.Execute()
+	if err != nil {
+		log.Fatal("cmd.Execute err: %v", err)
+	}
+}
